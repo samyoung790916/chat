84d87d2900ac2659aa0f05c654e8d52d5731cc26
samyoung79 <samyoung79@naver.com>
2019-04-10 18:51:00 +0900

    backup

---
+++ sqlite/DBInterface.h
@@ -0,0 +1,17 @@
+//
+//  DBInterface.h
+//  chat
+//
+//  Created by samyoung79 on 10/04/2019.
+//  Copyright © 2019 samyoung79. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBInterface : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sqlite/DBInterface.m
@@ -0,0 +1,13 @@
+//
+//  DBInterface.m
+//  chat
+//
+//  Created by samyoung79 on 10/04/2019.
+//  Copyright © 2019 samyoung79. All rights reserved.
+//
+
+#import "DBInterface.h"
+
+@implementation DBInterface
+
+@end
